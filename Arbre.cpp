5a852c7f10605915777e854b9f32084f52c3c48c
zoumson <38358621+zoumson@users.noreply.github.com>
2023-01-07 10:54:29 +0800

    binary search tree merge

--- Arbre.cpp
+++ Arbre.cpp
@@ -22,7 +22,7 @@ namespace za
 				p = { tmp.val , remain };
 				exist = true;
 				break;
-				
+
 			}
 			vis[tmp.val] = true;
 			if (tmp.l != nullptr && !vis[tmp.l->val])
@@ -39,7 +39,7 @@ namespace za
 		std::cout << "Target: " << target << std::endl;
 		if (exist)
 		{
-			std::cout << "Found"<< std::endl;
+			std::cout << "Found" << std::endl;
 			std::cout << "Pair: ( " << p.first << " , " << p.second << " )" << std::endl;
 		}
 		else
@@ -56,7 +56,7 @@ namespace za
 		std::stack<TNodeI*> s;
 		TNodeI* curr = &racine;
 		std::vector<int> inOrderData;
-		while (curr != nullptr|| !s.empty())
+		while (curr != nullptr || !s.empty())
 		{
 			/* Reach the left most Node of the
 			   curr Node */
@@ -91,7 +91,7 @@ namespace za
 			if (summ < target)
 			{
 				l++;
-			}			
+			}
 			if (summ > target)
 			{
 				r--;
@@ -101,7 +101,7 @@ namespace za
 				exist = false;
 				break;
 			}
-			if(summ == target)
+			if (summ == target)
 			{
 				exist = true;
 				break;
@@ -120,22 +120,22 @@ namespace za
 			std::cout << "Not Found" << std::endl;
 		}
 		std::cout << "Done" << std::endl;
-		
 
-		
+
+
 	}
 
 	void Inorder1(TNodeI* racine, std::vector<int>& dat)
 	{
 		if (racine == nullptr)
 		{
-			return ;
+			return;
 		}
 
 		Inorder1(racine->l, dat);
 		dat.push_back(racine->val);
 		Inorder1(racine->r, dat);
-		
+
 		return;
 	}
 	void Preorder1(TNodeI* racine, std::vector<int>& dat)
@@ -172,7 +172,7 @@ namespace za
 		auto tmp = racine;
 
 		while (tmp != nullptr || !st.empty())
-		//while (!st.empty())
+			//while (!st.empty())
 		{
 			while (tmp != nullptr)
 			{
@@ -446,7 +446,7 @@ namespace za
 		std::map< GTNodeI*, int> dist1, dist2;
 
 		//first bfs
-		st1.push(racine); 
+		st1.push(racine);
 		vis1[racine] = true;
 		dist1[racine] = 1;
 		int longPath1 = -1, longPath2 = -1;
@@ -471,7 +471,7 @@ namespace za
 			}
 
 		}
-		
+
 
 		//second bfs
 		GTNodeI* s2 = s1;
@@ -523,7 +523,7 @@ namespace za
 		{
 			return false;
 		}
-		
+
 		if (racine->r != nullptr && racine->r->val != racine->val)
 		{
 			return false;
@@ -829,7 +829,7 @@ namespace za
 
 			return preData;
 
-		};		
+		};
 		auto postOrderTras = [&](TNodeI* racine)
 		{
 			std::stack<TNodeI*> s1, s2;
@@ -861,7 +861,7 @@ namespace za
 
 			return postOrder;
 
-		};		
+		};
 		auto levelOrderTras = [&](TNodeI* racine)
 		{
 			std::queue<TNodeI*>q;
@@ -966,40 +966,40 @@ namespace za
 			{
 				dat.push_back(d->val);
 			}
-			
+
 		}
 
 	}
 
 	void Levelorder3(TNodeI* racine, std::map<int, std::vector<TNodeI*>>& m)
 	{
-			std::queue<TNodeI*> q;
-			q.push(racine);
-			int level = 0;
-			while (!q.empty())
+		std::queue<TNodeI*> q;
+		q.push(racine);
+		int level = 0;
+		while (!q.empty())
+		{
+			int levelSize = size(q);
+			std::vector< TNodeI*> currLevelData;
+			for (int i = 0; i < levelSize; i++)
 			{
-				int levelSize = size(q);
-				std::vector< TNodeI*> currLevelData;
-				for (int i = 0; i < levelSize; i++)
+				auto tmp = q.front();
+				q.pop();
+				if (tmp->l != nullptr)
 				{
-					auto tmp = q.front();
-					q.pop();
-					if (tmp->l != nullptr)
-					{
-						q.push(tmp->l);
-					}
-
-					if (tmp->r != nullptr)
-					{
-						q.push(tmp->r);
-					}
-					currLevelData.push_back(tmp);
-
-
+					q.push(tmp->l);
 				}
-				m[level] = currLevelData;
-				++level;
-			};
+
+				if (tmp->r != nullptr)
+				{
+					q.push(tmp->r);
+				}
+				currLevelData.push_back(tmp);
+
+
+			}
+			m[level] = currLevelData;
+			++level;
+		};
 
 
 	}
@@ -1031,7 +1031,7 @@ namespace za
 				{
 					st.push(tmp->r);
 					parent[tmp->r] = tmp;
-				}				
+				}
 				if (tmp->l != nullptr)
 				{
 					st.push(tmp->l);
@@ -1080,6 +1080,690 @@ namespace za
 		if (isNodeInLeftSubTri || isNodeInRightSubTri)
 		{
 			ances.push_back(racine);
+		}
+	}
+
+	void TriEnCoding(TNodeI* racine, std::deque<bool>& enCodeBitList, std::deque<int>& enCodeDataList)
+	{
+		if (racine == nullptr)
+		{
+			enCodeBitList.push_back(0);
+			return;
+		}
+		else
+		{
+			enCodeBitList.push_back(1);
+			enCodeDataList.push_back(racine->val);
+			TriEnCoding(racine->l, enCodeBitList, enCodeDataList);
+			TriEnCoding(racine->r, enCodeBitList, enCodeDataList);
+
+		}
+	}
+
+	//try use deque
+	TNodeI* TriDeCoding(std::deque<bool>& encodeBitList, std::deque<int>& encodeDataList)
+	{
+		if (encodeBitList.size() == 0)
+		{
+			return nullptr;
+		}
+		else
+		{
+			bool currBit = encodeBitList.front();
+			encodeBitList.pop_front();
+
+			if (currBit == 1)
+			{
+				int nodeVal = encodeDataList.front();
+				encodeDataList.pop_front();
+				TNodeI* racine = new TNodeI(nodeVal);
+				racine->l = TriDeCoding(encodeBitList, encodeDataList);
+				racine->r = TriDeCoding(encodeBitList, encodeDataList);
+				return racine;
+			}
+
+		}
+		return nullptr;
+	}
+
+	std::string SerializeArray(std::string s[], int l)
+	{
+		std::string encodedStr = "";
+		for (int i = 0; i < l; i++)
+		{
+			int l = s[i].length();
+			//same as std::to_string(l)
+			encodedStr.push_back('0' + l);
+			encodedStr = encodedStr + "-" + s[i];
+		}
+		return encodedStr;
+	}
+
+	std::string DeserializeArray(std::string s, std::string d[], int l)
+	{
+		int length, position = 0;
+		std::string t = "";
+		int i = 0;
+		while (position > -1)
+		{
+			position = s.find("-", position + 1);
+			if (position > 0)
+			{
+				length = s[position - 1] - 48;
+				t.append(s, position + 1, length);
+				d[i++] = t;
+				t = "";
+			}
+		}
+		//no return value before
+		//check
+		return t;
+	}
+
+	void TriSerializeHelper1(std::stringstream& ss, TNodeI* racine)
+	{
+		if (racine == nullptr)
+		{
+			ss << "#";
+			return;
+		}
+		ss << racine->val;
+		TriSerializeHelper1(ss, racine->l);
+		TriSerializeHelper1(ss, racine->r);
+	}
+	std::string TriSerialize1(TNodeI* racine)
+	{
+		std:: stringstream ss;
+		TriSerializeHelper1(ss, racine);
+		return ss.str();
+	}
+
+	void TriDeSerializeHelper1(std::stringstream& ss, TNodeI* cur)
+	{
+		std::string Node;
+		ss >> Node;
+		if (Node == "" || Node == "#") 
+		{
+			cur = nullptr;
+			return;
+		}
+		std::stringstream sss(Node);
+		int data;
+		sss >> data;
+		cur = new TNodeI(data);
+		cur->l = cur->r = nullptr;
+		TriDeSerializeHelper1(ss, cur->l);
+		TriDeSerializeHelper1(ss, cur->r);
+	}
+	TNodeI* TriDeSerialize1(std::string data)
+	{
+		std::stringstream ss(data);
+		TNodeI* racine = nullptr;
+		TriDeSerializeHelper1(ss, racine);
+		return racine;
+	}
+
+	void TriSerializeHelper2(TNodeI* racine, std::ostringstream& os)
+	{
+		if (racine == nullptr) 
+		{
+			os << "X" << " ";
+			return;
+		}
+		os << racine->val;
+		os << " ";
+		TriSerializeHelper2(racine->l, os);
+		TriSerializeHelper2(racine->r, os);
+	}
+	std::string TriSerialize2(TNodeI* racine)
+	{
+		std::ostringstream oss;
+		TriSerializeHelper2(racine, oss);
+		return oss.str();
+	}
+	TNodeI* TriDeSerializeHelper2(std::istringstream& is)
+	{
+		std::string val;
+		is >> val;
+		if (val == "X")
+		{
+			return nullptr;
+		}
+		TNodeI* racine = new TNodeI(stoi(val));
+		racine->l = TriDeSerializeHelper2(is);
+		racine->r = TriDeSerializeHelper2(is);
+		return racine;
+	}
+	TNodeI* TriDeSerialize2(std::string data)
+	{
+		std::istringstream is(data);
+		return TriDeSerializeHelper2(is);
+	}
+
+	void TriZigZag1(TNodeI* racine, std::vector<int>& dat)
+	{
+
+		if (racine == nullptr)
+		{
+			return;
+		}
+
+
+
+		std::stack<TNodeI*> current;
+		std::stack<TNodeI*> next;
+		current.push(racine);
+		bool lefttoright = true;
+		while (!current.empty()) 
+		{
+			TNodeI* temp = current.top();
+			current.pop();
+			if (temp) 
+			{
+				dat.push_back(temp->val);
+				if (lefttoright) 
+				{
+					if (temp->l)
+					{
+						next.push(temp->l);
+					}
+					if (temp->r)
+					{
+						next.push(temp->r);
+					}
+				}
+				else 
+				{
+					if (temp->r)
+					{
+						next.push(temp->r);
+					}
+					if (temp->l)
+					{
+						next.push(temp->l);
+					}
+				}
+			}
+			if (current.empty()) 
+			{
+				lefttoright = !lefttoright;
+				std::swap(current, next);
+			}
+		}
+	}
+	void TriZigZag2(TNodeI* racine, std::vector<int>& dat)
+	{
+		std::deque<TNodeI*> q;
+		q.push_back(racine);
+		dat.push_back(racine->val);
+		TNodeI* temp;
+		int l = 1; //level
+		while (!q.empty()) 
+		{
+			int n = q.size();
+			for (int i = 0; i < n; i++) 
+			{
+				if (l % 2 == 0) 
+				{
+					temp = q.back();
+					q.pop_back();
+				}
+				else 
+				{
+					temp = q.front();
+					q.pop_front();
+				}
+				if (l % 2 != 0) 
+				{
+					if (temp->r) 
+					{
+						q.push_back(temp->r);
+						dat.push_back(temp->r->val);
+					}
+					if (temp->l) 
+					{
+						q.push_back(temp->l);
+						dat.push_back(temp->l->val);
+					}
+				}
+				else if (l % 2 == 0) 
+				{
+					if (temp->l) 
+					{
+						q.push_front(temp->l);
+						dat.push_back(temp->l->val);
+					}
+					if (temp->r) 
+					{
+						q.push_front(temp->r);
+						dat.push_back(temp->r->val);
+					}
+				}
+			}
+			l++;
+		}
+	}
+	void TriZigZag3Level(TNodeI* racine, int level, bool l, std::vector<int>& dat)
+	{
+		if (racine == nullptr)
+		{
+			return;
+		}
+			
+		if (level == 1)
+		{
+			dat.push_back(racine->val);
+		}
+		else if (level > 1)
+		{
+			if (l)
+			{
+				TriZigZag3Level(racine->l, level - 1, l, dat);
+				TriZigZag3Level(racine->r, level - 1, l, dat);
+			}
+			else
+			{
+				TriZigZag3Level(racine->r, level - 1, l, dat);
+				TriZigZag3Level(racine->l, level - 1, l, dat);
+			}
+		}
+	}
+	int TriZigZag3Height(TNodeI* racine)
+	{
+		if (racine == nullptr)
+		{
+			return 0;
+		}
+		else
+		{
+			int left = TriZigZag3Height(racine->l);
+			int right = TriZigZag3Height(racine->r);
+			if (left > right)
+			{
+				return (left + 1);
+			}
+			else
+			{
+				return (right + 1);
+			}
+		}
+	}
+	void TriZigZag3(TNodeI* racine, std::vector<int>& dat)
+	{
+		int h = TriZigZag3Height(racine);
+		int i;
+		bool ltr = false;
+		for (i = 1; i <= h; i++)
+		{
+			TriZigZag3Level(racine, i, ltr, dat);
+			ltr = !ltr;
+		}
+	}
+	void TriZigZag4(TNodeI* racine, std::vector<int>& dat)
+	{
+		if (racine == nullptr)
+		{
+			return;
+		}
+		std::stack<TNodeI*> s1;
+		std::stack<TNodeI*> s2;
+		s1.push(racine);
+		while (!s1.empty() || !s2.empty()) 
+		{
+			while (!s1.empty()) 
+			{
+				TNodeI* temp = s1.top();
+				s1.pop();
+				dat.push_back(temp->val);
+				if (temp->r)
+				{
+					s2.push(temp->r);
+				}
+				if (temp->l)
+				{
+					s2.push(temp->l);
+				}
+			}
+			while (!s2.empty()) 
+			{
+				TNodeI* temp = s2.top();
+				s2.pop();
+				dat.push_back(temp->val);
+				if (temp->l)
+				{
+					s2.push(temp->l);
+				}
+				if (temp->r)
+				{
+					s2.push(temp->r);
+				}
+			}
+		}
+	}
+	void TriFoldable1Mirror(TNodeI* racine)
+	{
+		if (racine == nullptr)
+		{
+			return;
+		}
+		else 
+		{
+			TNodeI* temp;
+			TriFoldable1Mirror(racine->l);
+			TriFoldable1Mirror(racine->r);
+
+			temp = racine->l;
+			racine->l = racine->r;
+			racine->r = temp;
+		}
+	}
+	bool TriFoldable1Same(TNodeI* node1, TNodeI* node2)
+	{
+		if (node1 == nullptr && node2 == nullptr)
+		{
+			return true;
+		}
+		if (node1 != nullptr && node2 != nullptr &&
+			TriFoldable1Same(node1->l, node2->l) &&
+			TriFoldable1Same(node1->r, node2->r))
+		{
+			return true;
+		}
+
+		return false;
+	}
+	bool TriFoldable1(TNodeI* racine)
+	{
+		bool res;
+		if (racine == nullptr)
+		{
+			return true;
+		}
+		TriFoldable1Mirror(racine->l);
+		res = TriFoldable1Same(racine->l, racine->r);
+		TriFoldable1Mirror(racine->l);
+		return res;
+	}
+	bool TriFoldable2Check(TNodeI* node1, TNodeI* node2)
+	{
+		if (node1 == nullptr && node2 == nullptr) 
+		{
+			return true;
+		}
+		if (node1 == nullptr || node2 == nullptr) 
+		{
+			return false;
+		}
+		return TriFoldable2Check(node1->l, node2->r)
+			&& TriFoldable2Check(node1->r, node2->l);
+	}
+	bool TriFoldable2(TNodeI* racine)
+	{
+		if (racine == nullptr) 
+		{
+			return true;
+		}
+
+		return TriFoldable2Check(racine->l, racine->r);
+	}
+	bool TriIsomorphic1(TNodeI* racine1, TNodeI* racine2)
+	{
+		if (racine1 == nullptr && racine2 == nullptr)
+		{
+			return true;
+		}
+		if (racine1 == nullptr || racine2 == nullptr)
+		{
+			return false;
+		}
+		if (racine1->val != racine2->val)
+		{
+			return false;
+		}
+		return
+			(TriIsomorphic1(racine1->l, racine2->l) 
+				&& TriIsomorphic1(racine1->r, racine2->r)) 
+			|| (TriIsomorphic1(racine1->l, racine2->r)
+			&& TriIsomorphic1(racine1->r, racine2->l));
+	}
+	bool TriIsomorphic2(TNodeI* racine1, TNodeI* racine2)
+	{
+		if (racine1 == nullptr && racine2 == nullptr)
+		{
+			return true;
+		}
+		else if (racine1 == nullptr || racine2 == nullptr)
+		{
+			return false;
+		}
+		std::queue<TNodeI*> q1, q2;
+
+		q1.push(racine1);
+		q2.push(racine2);
+
+		int level = 0, size;
+		std::vector<int> v2;
+		std::unordered_map<int, int> mp;
+
+		while (!q1.empty() && !q2.empty()) 
+		{
+			if (q1.size() != q2.size())
+			{
+				return false;
+			}
+			size = q1.size();
+			level++;
+			v2.clear();
+			mp.clear();
+			while (size--) 
+			{
+				TNodeI* temp1 = q1.front();
+				TNodeI* temp2 = q2.front();
+				q1.pop();
+				q2.pop();
+
+				if (mp.find(temp1->val) == mp.end())
+				{
+					mp[temp1->val] = 1;
+				}
+				else
+				{
+					mp[temp1->val]++;
+				}
+
+				v2.push_back(temp2->val);
+				if (temp1->l)
+				{
+					q1.push(temp1->l);
+				}
+				if (temp1->r)
+				{
+					q1.push(temp1->r);
+				}
+				if (temp2->l)
+				{
+					q2.push(temp2->l);
+				}
+
+				if (temp2->r)
+				{
+					q2.push(temp2->r);
+				}
+			}
+			for (auto i : v2) 
+			{
+				if (mp.find(i) == mp.end())
+				{
+					return false;
+				}
+				else 
+				{
+					mp[i]--;
+					if (mp[i] < 0)
+					{
+						return false;
+					}
+					else if (mp[i] == 0)
+					{
+						mp.erase(i);
+					}
+				}
+			}
+			if (mp.size() != 0)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+
+	TNodeI* TriConvertCircularDoublyLinkedList2(TNodeI* racine, TNodeI** head, TNodeI** tail)
+	{
+		if (racine == nullptr)
+		{
+			return nullptr;
+		}
+		TNodeI* left = racine->l;
+		TNodeI* right = racine->r;
+		TriConvertCircularDoublyLinkedList2(racine->l, head, tail);
+
+		if (*head == nullptr) 
+		{
+			*head = racine;
+		}
+		racine->l = *tail;
+
+		if (*tail != NULL) 
+		{
+			(*tail)->r = racine;
+		}
+
+		*tail = racine;
+		TriConvertCircularDoublyLinkedList2(racine->r, head, tail);
+		return racine;
+	}
+	TNodeI* TriCircularDoublyLinkedList2(TNodeI* racine)
+	{
+		if (racine == nullptr)
+		{
+			return racine;
+		}
+		TNodeI* head = NULL;
+		TNodeI* tail = NULL;
+		TriConvertCircularDoublyLinkedList2(racine, &head, &tail);
+		return head;
+	}
+	void TriCircularDoublyLinkedList4(TNodeI* racine, TNodeI** head)
+	{
+		if (racine == nullptr)
+		{
+			return;
+		}
+		static TNodeI* prev = nullptr;
+
+		TriCircularDoublyLinkedList4(racine->l, head);
+		if (prev == nullptr)
+		{
+			*head = racine;
+		}
+		else
+		{
+			racine->l = prev;
+			prev->r = racine;
+		}
+		prev = racine;
+		TriCircularDoublyLinkedList4(racine->r, head);
+	}
+
+	void TriLeftSkewed(TNodeI* racine, std::vector<int> values)
+	{
+		TNodeI* prev, * curr;
+		prev = racine;
+		for (auto val : values)
+		{
+			curr = new TNodeI(val);
+			prev->l = curr;
+		}
+	}
+	void TriRightSkewed(TNodeI* racine, std::vector<int> values)
+	{
+		TNodeI* prev, *curr;
+		prev = racine;
+		for (auto val : values)
+		{
+			curr = new TNodeI(val);
+			prev->r = curr;
+		}
+	}
+
+	bool TriSkewedCheck(TNodeI* racine)
+	{
+		//This condition checks if the given is null node or leaf node
+		if (racine == nullptr || (racine->l == nullptr &&
+			racine->r == nullptr))
+		{
+			return true;
+		}
+
+		//Now, we check if the node has two children, if yes then we return false 
+		if (racine->l && racine->r)
+		{
+			return false;
+		}
+
+		//If the node has only one left child node then we check its left child node 
+		//else we check its right root node
+		if (racine->l)
+		{
+			return TriSkewedCheck(racine->l);
+		}
+		return TriSkewedCheck(racine->r);
+	}
+	void TriSkewedConv(TNodeI* racine, int k)
+	{
+		// Base Case
+		if (!racine)
+		{
+			return;
+		}
+
+		// To check the order in which the skewed tree should be made
+		if (k)
+		{
+			TriSkewedConv(racine->r, k);
+		}
+		else
+		{
+			TriSkewedConv(racine->l, k);
+		}
+
+		TNodeI* rightNode = racine->r;
+		TNodeI* leftNode = racine->l;
+
+		// To check if the root Node of the skewed tree is not defined
+		// //double check
+		//if (!headNode)
+		//{
+		//	headNode = racine;
+		//	racine->l = nullptr;
+		//	prevNode = racine;
+		//}
+		//else
+		//{
+		//	prevNode->r = racine;
+		//	root->l = nullptr;
+		//	prevNode = racine;
+		//}
+
+		// Now we will Recurse for the left or right subtree on the basis of the order
+		if (k)
+		{
+			TriSkewedConv(leftNode, k);
+		}
+		else
+		{
+			TriSkewedConv(rightNode, k);
 		}
 	}
 }
